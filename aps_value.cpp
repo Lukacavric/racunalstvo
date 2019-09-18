2da898b20dc540aec675af3a99edee62e8623be1
Lukacavric <55491650+Lukacavric@users.noreply.github.com>
2019-09-18 14:12:59 +0200

    Add files via upload

---
+++ aps_value.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cout<<"Upisi cjeli broj razlicit od 0:";
+	cin>>a;
+	if (a>0){
+	cout<<"njegova apsolutna vrijednost je: "<<a<<endl;
+	return 0;
+}
+if (a<0){
+	cout<<"njegova apsolutna vrijednost je: "<<-a<<endl;
+}
+}
